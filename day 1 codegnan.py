199a43ba89c158e2c50c106980be1cdf924396f2
kalapalamahima <78736544+kalapalamahima@users.noreply.github.com>
2026-04-15 21:31:54 +0530

    Add files via upload

---
+++ day 1 codegnan.py
@@ -0,0 +1,28 @@
+'''what is python?
+-->High level language(No need to worry about ll management like memory allocation)
+-->It never saves permenantely, but only save at run time.
+-->Object-oriented language(oops)
+-->Dynamically-typed language(no need to mention data type as c)
+'''
+
+a = int(input('Enter a number: '))
+b = int(input('Enter a number: '))
+print(f'a = {a}')
+print(f'b = {b}')
+print(a+b)
+
+
+a = 2
+b = 8
+print(id(a))
+print(id(b))
+
+
+a = 0
+b = python
+print(type(a))
+print(type(b))
+
+
+
+      
