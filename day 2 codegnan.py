fe440a3fbb20fff8a607349536d3829c4179dc06
kalapalamahima <78736544+kalapalamahima@users.noreply.github.com>
2026-04-16 10:37:04 +0530

    Add files via upload
    Day 2 :
    >How to declare a Variable
    >Keywords
    >Comments Types
    >Boolean type
    >Even and odd code

---
+++ day 2 codegnan.py
@@ -0,0 +1,90 @@
+''' Variable - A container which is used to hold a value
+--> Rules:(Not to do)
+1) They cant be starting with a number
+2) No special characters(space)
+
+--> Rules ( To do )
+.Either with letters or underscore
+.Should be defined with a meaningful name
+
+-->Keywords:(Identifiers)
+.Should not use the 36 of them as variable names(if,else,while,for,etc).
+
+-->Multiple variables- Should define for each variable mentioned
+
+-->Tokens - It is the small program to complete the task
+
+--> Comments(2 types)- Used to explain a code in a descriptive way for the future use.
+.Single line comment(#)
+.Multi line comment
+
+-->Boolean type:
+,.To find True or False
+
+
+v = 0
+print(v)
+
+num = 8
+num = 0
+
+_day = 07
+day = 07
+day_3 = 09
+
+
+
+
+'''
+# Defining a variable
+v = 0
+print(v)
+
+
+
+# Wrong way to declare a variable
+9num = 8
+ num = 0
+ 
+ 
+# Right way to declare a variable
+day3 = 07
+day = 07
+day_3 = 09
+
+
+
+# Multiple varible,Token
+a,b,c = 4,6,7
+print(a,b,c)
+print(a + b)
+
+
+
+# Even and odd
+num = int(input('Enter a number: '))
+if num%2 == 0:
+    print(f'{num} is even')
+else:
+      print(f'{num} is odd')
+      
+
+#Checking Boolean 
+num01 = 90
+num02 = 74
+print(num01 == num02)
+print(num01 != num02)
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
